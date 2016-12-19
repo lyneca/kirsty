0743cf69f919d95497de84ad98a3f308810e06ae
Lyneca <atraguliauniliantauthr@gmail.com>
2016-12-19 13:32:51 +1100

    added jumping

--- main.lua
+++ main.lua
@@ -9,9 +9,21 @@ player = {
   h = 70,
   vx = 0,
   vy = 0,
+  jump_speed = 6,
   speed = 4,
   is_touching_ground = false,
 }
+debug = ''
+function love.keypressed(key)
+  if key == 'up' then player.jump() end
+end
+
+function player.jump()
+  debug = 'jumped'
+  if player.is_touching_ground then
+    player.vy = -player.jump_speed
+  end
+end
 
 function player.update(dt)
   if love.keyboard.isDown('left') then
@@ -45,5 +57,6 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.print(tostring(player.is_touching_ground), 0, 0)
   love.graphics.rectangle('fill', player.x, player.y, player.w, player.h)
 end
